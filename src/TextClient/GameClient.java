93efa2f660f832ac4c0e04c81492e00f654989e8
Joel Cranston <Joel.Cranston@gmail.com>
2015-10-02 15:42:30 -0700

    Updated license

--- src/TextClient/GameClient.java
+++ src/TextClient/GameClient.java
@@ -1,7 +1,27 @@
 /*
- * Text based Game Client
- * Copyright (c) 2014, Joel Cranston. All rights reserved.
+ * The MIT License
+ *
+ * Copyright 2015 Joel Cranston.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
+
 package TextClient;
 
 import java.io.BufferedReader;
@@ -13,8 +33,8 @@ import java.net.Socket;
 import java.util.Timer;
 import java.util.TimerTask;
 
-/**
- * Text Game client
+/** 
+ * Text based Game Client
  * @author joel
  */
 public class GameClient {
